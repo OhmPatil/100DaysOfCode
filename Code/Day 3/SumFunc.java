98b5ce041db750ccdd261b0fffc01cee475c3fc9
OhmPatil <workpatil10@gmail.com>
2021-10-27 10:04:55 +0530

    Create SumFunc.java

---
+++ Code/Day 3/SumFunc.java
@@ -0,0 +1,21 @@
+package com.ohm;
+
+import java.util.Scanner;
+
+public class SumFunc {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter two numbers: ");
+        float num1 = in.nextFloat();
+        float num2 = in.nextFloat();
+        float res = sum(num1 , num2);
+        System.out.println("Sum is: "+res);
+    }
+
+    static float sum(float a, float b){
+        float ans = a + b;
+        return ans;
+    }
+}
+
